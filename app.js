7cf100a80a3efbd6ac2306d56b4b0d6cef6906e2
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-23 12:49:53 -0700

    trying to show beanies

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 /* Imports */
+import { getBeanies, getAstroSigns } from '.fetch-utils.js';
+import { renderAstroSignsOption, renderBeanie } from './render-utils.js';
 
 /* Get DOM Elements */
+const notificationDisplay = document.getElementById('notification-display');
+const beanieList = document.getElementById('beanie-list');
 
 /* State */
+let error = null;
+let count = 0;
+let astroSigns = [];
+let beanie = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getAstroSigns();
+    error = response.error;
+    astroSigns = response.data;
+
+  
+
+    async function findBeanies(name, astroSign) {
+        const response = await getBeanies(name, astroSign);
+        
+        error.response.error;
+        count.response.count;
+        beanieList.response.data;
+    }
+    
+
 
 /* Display Functions */
 
--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,11 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getBeanies(name, astrosign) {
+    let query = client
+        .from('beanie_babies')
+        .select('*', { count: 'exact' })
+        .order('releaseYear')
+        .limit(100);
+}
