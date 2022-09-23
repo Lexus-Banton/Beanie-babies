21a425552eb960d7601bba19cdd0081644474e56
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-23 16:54:08 -0700

    rendered beanies

--- render-utils.js
+++ render-utils.js
@@ -4,3 +4,14 @@ export function renderAstroSigns(astroSign) {
     sign.textContent = astroSign.name;
     return sign;
 }
+
+export function renderBeanie(beanie) {
+    const baby = document.createElement('li');
+    const title = document.createElement('h2');
+    const year = document.createElement('p');
+    const astro = document.createElement('p');
+    const image = document.createElement('img');
+    image.src = beanie.image;
+    baby.append(title, year, astro, image);
+    return baby;
+}
